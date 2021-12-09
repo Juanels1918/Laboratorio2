e3b696eef2e9e4f2c06da8d5888e05f16f667661
ELFish998 <lv.villalobos1998@gmail.com>
2021-12-09 14:17:23 -0500

    launcher
    launcher

---
+++ src/launcher/Launcher.java
@@ -0,0 +1,17 @@
+package launcher;
+
+
+
+public class Launcher {
+
+	
+	public static void main(String[] args) {
+
+		try {
+		
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
