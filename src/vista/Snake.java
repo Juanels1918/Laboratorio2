c7f7e7240367419723242d9cbad2321197768945
esteban <esteban@esteban-System-Product-Name>
2021-12-09 13:36:21 -0500

    Creación repo

---
+++ src/vista/Snake.java
@@ -0,0 +1,48 @@
+package vista;
+
+import java.awt.BorderLayout; 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Snake extends JFrame {
+
+	private JPanel contentPane;
+	int ancho = 460;
+	int alto = 480;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Snake frame = new Snake();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Snake() {
+		
+		setTitle("Snake");
+		setSize(ancho,alto);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
